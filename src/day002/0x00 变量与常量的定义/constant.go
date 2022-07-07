8a42cbfcc7576ea1694d8590fafc07ad8bffcc63
lkeme <1462364497@qq.com>
2022-07-07 15:56:10 +0800

    [fix(*)] day002 0x0

--- src/day002/0x00 变量与常量的定义/constant.go
+++ src/day002/0x00 变量与常量的定义/constant.go
@@ -1,7 +1,38 @@
 package main
 
+import "fmt"
+
 const pi = 3.1415926
 
-func main() {
+const (
+	a    = iota // 0
+	b           // 1
+	c    = 100  // 100
+	d           // 注意这里已经中断 是参考上一个值  是100
+	e    = iota // 再次使用iota 继续iota枚举 所以是4
+	f, g = iota, iota
+)
 
+const (
+	j = iota + 100
+	k
+	l
+)
+
+const (
+	x = iota + 3
+	xx
+	xxx
+	y = iota + 6
+	yy
+	yyy
+	z = iota + 9
+	zz
+	zzz
+)
+
+func main() {
+	fmt.Println(a, b, c, d, e, f, g)
+	fmt.Println(j, k, l)
+	fmt.Println(x, xx, xxx, y, yy, yyy, z, zz, zzz)
 }
